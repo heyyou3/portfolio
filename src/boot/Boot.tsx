969d86d1f7585e64eefc17b16bace308153a430e
heyyou <hey3you3@gmail.com>
2021-08-01 11:59:59 +0900

    feat: create Boot Display

--- src/boot/Boot.tsx
+++ src/boot/Boot.tsx
@@ -1,16 +1,52 @@
-import React from "react";
+import React from 'react';
+import { useEffect } from 'react';
+import {useState} from 'react';
 import './Boot.css';
 
-class Boot extends React.Component {
-  render() {
-    return (
-      <div className="boot">
-        <div>Welcome to heyyou Portfolio!</div>
-        <br />
-        <div>Start Boot...</div>
-      </div>
-    );
-  }
-}
+const bootMessages = [
+  'Welcome to heyyou Portfolio!',
+  '...',
+  'Start Boot',
+  '...',
+  '...',
+  'Set heyyou Profile',
+  'Set GitHub User  heyyou3',
+  'Set Qiita User hey1you1',
+  '...',
+  '...',
+  '...',
+  'Running heyyou Portfolio!',
+];
+
+const Boot: React.FC = () => {
+  const [displayMessages, setDisplayMessages] = useState<String[]>([]);
+  const [cnt, setCnt] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      if  (bootMessages.length === cnt) {
+        clearInterval(id);
+      }
+      if (bootMessages.length !== cnt)  {
+        setCnt(c => c + 1);
+        setDisplayMessages(dm => {
+          return [...dm, bootMessages[cnt]];
+        });
+      }
+    }, 300);
+    return () => clearInterval(id);
+  }, [cnt]);
+
+
+  return (
+    <div className="boot">
+      {
+        displayMessages.map((msg, i) => (
+          <div key={i.toString()}>{msg}</div>
+        ))
+      }
+    </div>
+  );
+};
 
 export default Boot;
